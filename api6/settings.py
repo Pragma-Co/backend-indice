849d0d682cd48a8edc1cfb2f0c0b2a3612e6a417
BispoJPM <marcondesjpb@gmail.com>
2026-09-08 15:52:54 -0300

    chore(#8): add temp upload size and directory settings

--- api6/settings.py
+++ api6/settings.py
@@ -99,6 +99,13 @@ MONGO_PASSWORD = os.environ.get("MONGO_PASSWORD", "")
 MONGO_HOST = os.environ.get("MONGO_HOST", "localhost")
 MONGO_PORT = os.environ.get("MONGO_PORT", "27018")
 
+MAX_UPLOAD_SIZE_BYTES = int(
+    os.environ.get("MAX_UPLOAD_SIZE_BYTES", 100 * 1024 * 1024)
+)
+TEMP_UPLOAD_DIR = Path(
+    os.environ.get("TEMP_UPLOAD_DIR", BASE_DIR / "tmp_uploads")
+)
+
 # Password validation (also part of a healthy LGPD posture)
 
 AUTH_PASSWORD_VALIDATORS = [
